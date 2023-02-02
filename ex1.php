6895992bdcb1de1b1a15f47d849ff278136bda52
Eusabious12 <eusabious22000@student.hamk.fi>
2023-02-02 23:54:10 +0200

    Initial commit

---
+++ ex1.php
@@ -0,0 +1,19 @@
+<?php include "header.php" ?>
+
+<h1>Write a simple PHP script to print your information (Eusabious, Team 19)</h1>
+<?php
+
+echo "Eusabious SJ <br>"; 
+echo "BBCAP22";
+
+?>
+<h2> Hello world! My name is Eusabious. </h2>
+
+<?php
+echo "this is a test website";
+?>
+
+<h3> Current Date </h3>
+<?php echo date("d.m.Y") ?>
+
+<?php include "footer.php"?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include "header.php" ?>
+
+<h1> Some content </h1>
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio laboriosam illo quo mollitia, minus modi nobis dolores, praesentium sed cum similique, sint officiis ab numquam in aspernatur assumenda sequi harum.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, animi. Quae nemo iste delectus veritatis mollitia, magnam error ipsa perspiciatis quo! Reprehenderit consequatur libero repellat rerum voluptates sequi consequuntur quidem.
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt eius error reiciendis illo animi quos, quis perferendis dolorem quibusdam velit eligendi nisi maiores hic, sed saepe eos quaerat qui quas?
+
+</p>
+<p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores voluptatem hic commodi ratione ipsum ipsam veritatis, libero, fugiat magnam laudantium labore. Veritatis rerum provident incidunt ut, adipisci quis neque ipsum?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dicta velit nam minus itaque fugit esse, reprehenderit expedita minima. Repellat eveniet cumque eos facilis recusandae ex commodi quod error quaerat.
+
+
+</p>
+
+<?php include "header.php" ?>
